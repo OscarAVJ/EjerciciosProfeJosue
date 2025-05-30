e88b1164a05f7884a989bafe841540c31770fade
AbelVJ <oscar.joyar@gmail.com>
2025-05-30 15:14:50 -0600

    Ejercicio 7

---
+++ Ejercicio7Repetitiva/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int num = 1; 10>= num; num++){
+            System.out.println(num);
+        }
+    }
+}
